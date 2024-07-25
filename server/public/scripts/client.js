429cfb8f0004187131acb6930e3532446f7fcf1c
Daniel Richter <drichter3697@gmail.com>
2024-07-25 18:08:27 -0500

    added get and post routes

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,46 @@
-console.log('JS is sourced!');
+function onReady(){
+    console.log('JS is sourced!');
+    getItemsToDo()
+}
+//make a get route
+function getItemsToDo(){
+    console.log(`in getItemsToDo! Let's get some items!`)
+    axios({
+        method: `GET`,
+        url: `/todos`
+    }).then((response) =>{
+        console.log(`Got something back in my GET /todos!`, response)
+        document.getElementById(`toDoListTable`).innerHTML = ``
+        for(let item of response.data){
+            document.getElementById(`toDoListTable`).innerHTML += `
+            <td data-testid="toDoItem">${item.text}</td>
+            <td>Completion status(tobe implemented haha)</td>`
+        }
+    }).catch((error) =>{
+    console.log(`Oopsie Woopsie! Error in GET route!`, error)
+    })
+}
+
+//make a post route
+function postToDoForm(e){
+    e.preventDefault()
+    console.log(`postToDoForm recieved a request!`)
+    let itemToPost = document.getElementById(`toDoFormText`).value
+    axios({
+        method: `POST`,
+        url: `/todos`,
+        data: {text: itemToPost}
+    }).then((response) =>{
+        document.getElementById(`toDoFormText`).value = ``
+        getItemsToDo()
+    }).catch((error) =>{
+        console.log(`Oopsie! Recieved an error in the POST/todos!`, error)
+    })
+    //
+}
+//make a patch route
+//make a delete route
+
+
+
+onReady()
--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -1,7 +1,31 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
 
-
+router.get(`/`, (req, res) =>{
+    const sqlText = `SELECT * FROM "todos";
+    `
+    pool.query(sqlText)
+    .then((dbResponse) =>{
+        res.send(dbResponse.rows)
+    }).catch((dbError) =>{
+        console.log(`SQL Query error in /todos!`, dbError)
+        res.sendStatus(500)
+    })
+})
+router.post(`/`, (req, res) =>{
+    const sqlText = `INSERT INTO "todos"
+  ("text")
+  VALUES 
+  ($1);`
+  const sqlValues = [req.body.text]
+  pool.query(sqlText, sqlValues)
+  .then((dbResponse) =>{
+    res.sendStatus(201)
+  }).catch((dbErr) =>{
+    console.log(`SQL Error in POST/todos!`, dbErr)
+    res.sendStatus(500)
+  })
+})
 
 
 module.exports = router;
