4f3e9d48b15f05a82465e0c0c31fa2e79a88f680
Daniel Richter <drichter3697@gmail.com>
2024-07-26 09:13:00 -0500

    added in the ability to toggle completed status of a todo item

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,44 +1,63 @@
-function onReady(){
+function onReady() {
     console.log('JS is sourced!');
     getItemsToDo()
 }
 //make a get route
-function getItemsToDo(){
+function getItemsToDo() {
     console.log(`in getItemsToDo! Let's get some items!`)
     axios({
         method: `GET`,
         url: `/todos`
-    }).then((response) =>{
+    }).then((response) => {
         console.log(`Got something back in my GET /todos!`, response)
         document.getElementById(`toDoListTable`).innerHTML = ``
-        for(let item of response.data){
+        for (let item of response.data) {
+            if(item.isComplete){
             document.getElementById(`toDoListTable`).innerHTML += `
             <td data-testid="toDoItem">${item.text}</td>
-            <td>Completion status(tobe implemented haha)</td>`
+            <td onClick="toggleCompleted('complete', ${item.id})" class="completed">Completed!</td>`
+        } else{
+            document.getElementById(`toDoListTable`).innerHTML += `
+            <td data-testid="toDoItem">${item.text}</td>
+            <td onClick="toggleCompleted('incomplete', ${item.id})"class="incomplete">Incomplete</td>`
         }
-    }).catch((error) =>{
-    console.log(`Oopsie Woopsie! Error in GET route!`, error)
+        }
+    }).catch((error) => {
+        console.log(`Oopsie Woopsie! Error in GET route!`, error)
     })
 }
 
 //make a post route
-function postToDoForm(e){
+function postToDoForm(e) {
     e.preventDefault()
     console.log(`postToDoForm recieved a request!`)
     let itemToPost = document.getElementById(`toDoFormText`).value
     axios({
         method: `POST`,
         url: `/todos`,
-        data: {text: itemToPost}
-    }).then((response) =>{
+        data: { text: itemToPost }
+    }).then((response) => {
         document.getElementById(`toDoFormText`).value = ``
         getItemsToDo()
-    }).catch((error) =>{
+    }).catch((error) => {
         console.log(`Oopsie! Recieved an error in the POST/todos!`, error)
     })
     //
 }
 //make a patch route
+function toggleCompleted(status, itemId){
+    console.log(`ToggleCompleted recieved a request!`, status, itemId)
+    axios({
+        method: 'PATCH',
+        url: `/todos`,
+        data: {status: status, itemId: itemId}
+    }).then((response) =>{
+        console.log(`Patch complete!`)
+        getItemsToDo()
+    }).catch((error) =>{
+        console.log(`Oopsie Woopsie! We had a fucky wuppy in the PATCH/todos!`, error)
+    })
+}
 //make a delete route
 
 
--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const pool = require('../modules/pool');
 
 router.get(`/`, (req, res) =>{
-    const sqlText = `SELECT * FROM "todos";
+    const sqlText = `SELECT * FROM "todos"
+     ORDER BY "id";
     `
     pool.query(sqlText)
     .then((dbResponse) =>{
@@ -27,5 +28,24 @@ router.post(`/`, (req, res) =>{
   })
 })
 
+router.patch(`/`, (req, res) => {
+  const sqlText = `UPDATE "todos"
+	  SET "isComplete" = $1
+	  WHERE "id" = $2;`
+  let newStatus
+  if(req.body.status === `incomplete`){
+    newStatus = true
+  } else if (req.body.status === `complete`){
+    newStatus = false
+  }
+  const sqlValues = [newStatus, req.body.itemId]
+  pool.query(sqlText, sqlValues)
+    .then((dbResponse) => {
+      res.sendStatus(200)
+    }).catch((dbErr) =>{
+      console.log(`Uh oh! SQL Error in PATCH/todos!`, dbErr)
+      res.sendStatus(500)
+    })
+})
 
 module.exports = router;
